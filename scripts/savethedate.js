9e12365faacd7ffe680d1e710c02f9c262ab4536
Tuuhl <tuuhl89@gmail.com>
2019-02-24 12:16:27 +0200

    Add files via upload

--- scripts/savethedate.js
+++ scripts/savethedate.js
@@ -7,7 +7,7 @@ var main = function() {
 	
 	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 	
-	$("#timer").text(days + " days ");
+	$("#timer").text(days + " days");
 	
 	$("#emblem").click(function(){
 		window.location.href='https://www.youtube.com/watch?v=dmM32uCVR_A';
